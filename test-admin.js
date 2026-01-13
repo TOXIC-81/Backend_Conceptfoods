951a6fe0d7ab1ce24155c6e6a96aefa6ed12c260
TOXIC-81 <181952821+TOXIC-81@users.noreply.github.com>
2026-01-13 12:35:23 +0530

    Add files via upload

--- test-admin.js
+++ test-admin.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 async function testAdminLogin() {
   try {
-    const response = await fetch('http://localhost:5000/api/admin/login', {
+    const response = await fetch('https://backend-conceptfoods.onrender.com/admin/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
