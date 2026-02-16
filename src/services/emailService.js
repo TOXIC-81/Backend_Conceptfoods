78cd1df3b694f8a55b41cbc21bfc36a6d29ed981
TOXIC-81 <181952821+TOXIC-81@users.noreply.github.com>
2026-02-16 15:33:10 +0530

    Add files via upload

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -3,9 +3,7 @@ import nodemailer from "nodemailer";
 class EmailService {
   constructor() {
     this.transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 587,
-      secure: false,
+      service: 'gmail',
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
